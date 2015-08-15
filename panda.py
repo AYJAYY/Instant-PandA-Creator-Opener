d45c52caf5cae1b941d1a479c027990dfb7b7128
AJBBB <ajbbb@mail.com>
2015-08-14 22:01:11 -0400

    Initial commit

--- panda.py
+++ panda.py
@@ -1,13 +1,12 @@
 import webbrowser
 
-while 1 == 1:
+while True:
     try:
         url_to_split = raw_input("Enter URL:")
         split = url_to_split.split("preview")
         link = split[0] + "previewandaccept" + split[1]
         new_tab = 2
         webbrowser.open(link, new=new_tab)
-
 
     except:
         print "Invalid URL"
