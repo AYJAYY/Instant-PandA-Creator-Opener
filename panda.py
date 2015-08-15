02ee29e9ab71397f7f625ad36d710d00b9c9402d
AJBBB <ajbbb@mail.com>
2015-08-15 00:18:23 -0400

    Initial commit + Comments

--- panda.py
+++ panda.py
@@ -2,10 +2,15 @@ import webbrowser
 
 while True:
     try:
+        # Get the Preview URL
         url_to_split = raw_input("Enter URL:")
+        # Split @ and remove preview
         split = url_to_split.split("preview")
+        # Create the PandA
         link = split[0] + "previewandaccept" + split[1]
+        # Make sure it opens in a new tab
         new_tab = 2
+        # Open the browser with the PandA
         webbrowser.open(link, new=new_tab)
 
     except:
